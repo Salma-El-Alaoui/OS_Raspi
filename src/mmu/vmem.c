e53c3f48e7e7de524f8bf6727678e24115cc412b
qdupont <df>
2014-12-12 11:22:28 +0100

    Initialisation à la main des valeurs de la mémoire (pas phyalloc)

--- src/mmu/vmem.c
+++ src/mmu/vmem.c
@@ -3,11 +3,13 @@
 	
 #define PAGE_SIZE			 (4*1024*8)// 4KB
 #define SECON_LVL_TT_COUN 	 256 // 2⁸=256 entrees au lvl 2
-#define SECON_LVL_TT_SIZE 	 ((SECON_LVL_TT_COUN * 32)/8) // taille du lvl 2 en octets
-#define FIRST_LVL_TT_COUN 	 4096 // 2^12=4096 entrees au lvl 1
-#define FIRST_LVL_TT_SIZE 	 ((FIRST_LVL_TT_COUN * 32)/8) // taille du lvl 1 en octets
-#define TOTAL_TT_SIZE 		 (SECON_LVL_TT_SIZE * FIRST_LVL_TT_COUN)
+#define SECON_LVL_TT_SIZE 	 ((SECON_LVL_TT_COUN * 32)/8) // taille du lvl 2 en octets = 1024 octets
+#define FIRST_LVL_TT_COUN 	 4096 // 2^12=4096 entrees au lvl 1 
+#define FIRST_LVL_TT_SIZE 	 ((FIRST_LVL_TT_COUN * 32)/8) // taille du lvl 1 en octets = 16384 octets
+#define TOTAL_TT_SIZE 		 (SECON_LVL_TT_SIZE * FIRST_LVL_TT_COUN + FIRST_LVL_TT_SIZE)
 
+
+uint32_t first_level_flags= 1; // 10 bits de flags, seul le dernier est à 1 (toujours)
 
 uint32_t device_flags= 
 	(0 << 12) | //nG 1 si entrée valide pour tous les processus
@@ -19,7 +21,10 @@ uint32_t device_flags=
 	(1<<1) | // Toujours 1
 	0 ; // XN 1 si contient du code exécutable
 	
-unsigned int pointer_pagetable_lvl1;
+unsigned int pointer_pagetable_lvl1=0x48000;
+unsigned int pointer_start_lvl2=0x52001; // adresse de départ de la table de niveau 2
+// Level 1 : 0x48000 -> 0x52000
+// Level 2 : 0x52001 -> 0x452001
 
 void start_mmu_C()
 {
@@ -58,34 +63,41 @@ void configure_mmu_C()
 }
 	
 unsigned int init_kern_translation_table (void) {	
-	// Allocation table de niveau 1
-	pointer_pagetable_lvl1=(unsigned int) phyAlloc_alloc(FIRST_LVL_TT_SIZE);
+	
+	// Espace logique 0xC0000...
+	// TODO flags
 	
 	int i=0;
+	int *adrLvl2=(int *)pointer_start_lvl2; 
 	
-	// Allocation des tables de niveau 2
-	for(i=pointer_pagetable_lvl1; i<pointer_pagetable_lvl1+FIRST_LVL_TT_SIZE; i++)
+	// Remplissage de la table de niveau 1
+	for(i=pointer_pagetable_lvl1; i<pointer_pagetable_lvl1+FIRST_LVL_TT_SIZE; i+=4)
 	{
-		int* tmp=i;
-		(*tmp)=(unsigned int) phyAlloc_alloc(SECON_LVL_TT_SIZE);
+		int** tmp=(int **)i;
+		(*tmp) =  (int*)(((int)adrLvl2 << 10) | first_level_flags);
+		adrLvl2 += SECON_LVL_TT_COUN;
 	}
 
 			
-	 // Adresses physiques entre 0x0 et 0x500000 -> p entre 0x0 et 0xA0001
-	for(i=0; i<0xA0001; i++)	{
+	 // Adresses physiques entre 0x0 et 0x500000 -> p entre 0x0 et 0x500
+	for(i=0; i<0x500; i++)	{	
 		int indexEntreeLvl1=i/256;
-		int* pointeurEntreeLvl1 = pointer_pagetable_lvl1 + indexEntreeLvl1;
-		int* entreeLvl2=(*pointeurEntreeLvl1);
+		int offsetLvl2 = i%256;
+		int** pointeurCaseLvl1= (int **)(pointer_pagetable_lvl1) + indexEntreeLvl1;
+		int* pointeurTableLvl2=(*pointeurCaseLvl1);
+		int* pointeurEntreeLvl2 = pointeurTableLvl2 + offsetLvl2;
 		
-		(*entreeLvl2)=i;
+		(*pointeurEntreeLvl2)=(i<< 13) | device_flags;
 	}
-	// Adresses physiques entre 0x20000000 et 0x20FFFFFF -> p entre 0x80000 et 0x83fff
-		for(i=0x80000; i<0x83fff; i++)	{
+	// Adresses physiques entre 0x20000000 et 0x20FFFFFF -> p entre 0x20000 et 0x20FFF
+		for(i=0x20000; i<0x20fff; i++)	{
 		int indexEntreeLvl1=i/256;
-		int* pointeurEntreeLvl1 = pointer_pagetable_lvl1 + indexEntreeLvl1;
-		int* entreeLvl2=(*pointeurEntreeLvl1);
+		int offsetLvl2 = i%256;
+		int** pointeurCaseLvl1= (int **)(pointer_pagetable_lvl1) + indexEntreeLvl1;
+		int* pointeurTableLvl2=(*pointeurCaseLvl1);
+		int* pointeurEntreeLvl2 = pointeurTableLvl2 + offsetLvl2;
 		
-		(*entreeLvl2)=i;
+		(*pointeurEntreeLvl2)=(i << 13) | device_flags;
 	}
 	
 	start_mmu_C();
