84b920f994d2d5c6fab10c0a34879272ba974423
qdupont <df>
2014-12-19 11:22:49 +0100

    Flags pour chaque niveau de la table des pages, start_mmu bug toujours

--- src/mmu/vmem.c
+++ src/mmu/vmem.c
@@ -8,29 +8,45 @@
 #define FIRST_LVL_TT_SIZE 	 ((FIRST_LVL_TT_COUN * 32)/8) // taille du lvl 1 en octets = 16384 octets
 #define TOTAL_TT_SIZE 		 (SECON_LVL_TT_SIZE * FIRST_LVL_TT_COUN + FIRST_LVL_TT_SIZE)
 
+static int total= TOTAL_TT_SIZE;
 
 uint32_t first_level_flags= 1; // 10 bits de flags, seul le dernier est à 1 (toujours)
 
+uint32_t normal_flags= 
+	(0 << 12) | //nG 1 si entrée valide pour tous les processus
+	(0 << 11) | //S 1 si entrée partagée
+	(0 << 10) | //APX (dépend des permissions)
+	(0b001 << 6) | //TEX (cf 8.4.2 du sujet)
+	(0b11 << 4) | //AP (dépend des permissions)
+	(0b00 << 2)| // CB (dépend de TEX)
+	(1<<1) | // Toujours 1
+	0 ; // XN 1 si contient du code exécutable
+	
 uint32_t device_flags= 
 	(0 << 12) | //nG 1 si entrée valide pour tous les processus
 	(0 << 11) | //S 1 si entrée partagée
 	(0 << 10) | //APX (dépend des permissions)
 	(0b000 << 6) | //TEX (cf 8.4.2 du sujet)
 	(0b11 << 4) | //AP (dépend des permissions)
-	(0b00 << 2)| // CB (dépend de TEX)
+	(0b01 << 2)| // CB (dépend de TEX)
 	(1<<1) | // Toujours 1
 	0 ; // XN 1 si contient du code exécutable
-	
-unsigned int pointer_pagetable_lvl1=0x48000;
-unsigned int pointer_start_lvl2=0x52001; // adresse de départ de la table de niveau 2
+
 // Level 1 : 0x48000 -> 0x52000
-// Level 2 : 0x52001 -> 0x452001
+// Level 2 : 0x52000 -> 0x452000	
+unsigned int pointer_pagetable_lvl1=0x48000;
+unsigned int pointer_start_lvl2=0x52000; // adresse de départ de la table de niveau 2
+
+// Table occupation des frame 0x452000 -> 0x473000
+unsigned int pointer_table_occup_frame=0x452000;
+unsigned int end_table_occup_frame=0x473000;
 
 void start_mmu_C()
 {
+	
 	register unsigned int control;
 	__asm("mcr p15, 0, %[zero], c1, c0, 0" : : [zero] "r"(0)); //Disable cache
-	__asm("mcr p15, 0, r0, c7, c7, 0"); //Invalidate cache (data and instructions) */
+	__asm("mcr p15, 0, r0, c7, c7, 0"); //Invalidate cache (data and instructions) 
 	__asm("mcr p15, 0, r0, c8, c7, 0"); //Invalidate TLB entries
 
 	/* Enable ARMv6 MMU features (disable sub-page AP) */
@@ -46,7 +62,7 @@ void start_mmu_C()
 void configure_mmu_C()
 {
 	register unsigned int pt_addr = pointer_pagetable_lvl1;
-	//total++;
+	total++;
 	/* Translation table 0 */
 	__asm volatile("mcr p15, 0, %[addr], c2, c0, 0" : : [addr] "r" (pt_addr));
 
@@ -61,47 +77,64 @@ void configure_mmu_C()
 	*/
 	__asm volatile("mcr p15, 0, %[r], c3, c0, 0" : : [r] "r" (0x3));
 }
+
+void init_table_occup_frame()
+{
+	uint8_t* i;
+	for(i=pointer_table_occup_frame; i<end_table_occup_frame; i++){
+		(*i) = 0; // default : frame is free
+	}
+}
+
+//uint8_t* vMem_Alloc(unsigned int nbPages){
+//}
+
+//void vMem_Free(uint8_t* ptr, unsigned int nbPages){
+//}
 	
 unsigned int init_kern_translation_table (void) {	
 	
-	// Espace logique 0xC0000...
-	// TODO flags
-	
 	int i=0;
-	int *adrLvl2=(int *)pointer_start_lvl2; 
+	int *adrLvl2=(int *)(pointer_start_lvl2); 
 	
 	// Remplissage de la table de niveau 1
 	for(i=pointer_pagetable_lvl1; i<pointer_pagetable_lvl1+FIRST_LVL_TT_SIZE; i+=4)
 	{
 		int** tmp=(int **)i;
-		(*tmp) =  (int*)(((int)adrLvl2 << 10) | first_level_flags);
+		// On enlève les 8 derniers bits qui correspondent au second-level table index pour obtenir la coarse page table base address
+		int baseAddrLvl2 = ((int)adrLvl2 >> 8); 
+		(*tmp) =  (int*)((baseAddrLvl2 << 10) | first_level_flags);
 		adrLvl2 += SECON_LVL_TT_COUN;
 	}
 
 			
 	 // Adresses physiques entre 0x0 et 0x500000 -> p entre 0x0 et 0x500
-	for(i=0; i<0x500; i++)	{	
+	for(i=0; i<0x500; i++)	{
 		int indexEntreeLvl1=i/256;
 		int offsetLvl2 = i%256;
-		int** pointeurCaseLvl1= (int **)(pointer_pagetable_lvl1) + indexEntreeLvl1;
-		int* pointeurTableLvl2=(*pointeurCaseLvl1);
+		int* pointeurCaseLvl1= (int *)(pointer_pagetable_lvl1) + indexEntreeLvl1;
+		// On enlève les 10 bits de first_level_flags et on rajoute les 8 bits de second-level table index (0 pour le début de la table)
+		int* pointeurTableLvl2=(int*)(((*pointeurCaseLvl1) >> 10) << 8 );
 		int* pointeurEntreeLvl2 = pointeurTableLvl2 + offsetLvl2;
 		
-		(*pointeurEntreeLvl2)=(i<< 13) | device_flags;
+		(*pointeurEntreeLvl2)=(i<< 13) | normal_flags;
 	}
 	// Adresses physiques entre 0x20000000 et 0x20FFFFFF -> p entre 0x20000 et 0x20FFF
 		for(i=0x20000; i<0x20fff; i++)	{
 		int indexEntreeLvl1=i/256;
 		int offsetLvl2 = i%256;
-		int** pointeurCaseLvl1= (int **)(pointer_pagetable_lvl1) + indexEntreeLvl1;
-		int* pointeurTableLvl2=(*pointeurCaseLvl1);
+		int* pointeurCaseLvl1= (int *)(pointer_pagetable_lvl1) + indexEntreeLvl1;
+		// On enlève les 10 bits de first_level_flags et on rajoute les 8 bits de second-level table index (0 pour le début de la table)
+		int* pointeurTableLvl2=(int*)(((*pointeurCaseLvl1) >> 10) << 8 );
 		int* pointeurEntreeLvl2 = pointeurTableLvl2 + offsetLvl2;
 		
 		(*pointeurEntreeLvl2)=(i << 13) | device_flags;
 	}
 	
-	start_mmu_C();
 	configure_mmu_C();
-			
+	//start_mmu_C();
+				
+	init_table_occup_frame();
+				
 	return 0;
 }
